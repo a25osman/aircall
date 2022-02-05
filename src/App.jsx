986259b068320c5ee3932c0fe6df1d1c04a375b6
Awab Osman <awabosman3@gmail.com>
2022-02-04 20:00:51 -0500

    add tabs for recents and archived

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header.jsx';
 import {useState, useEffect } from 'react';
 import axios from "axios";
 import CallList from "./components/CallList.jsx"
+import Header from './components/Header.jsx';
+
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import PhoneIcon from '@mui/icons-material/Phone';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import Box from '@mui/material/Box';
 
 
 const App = () => {
+  // State containing call data
   const [callLogs, setCallLogs] = useState([]);
 
+  // Retrieve call data from API
   useEffect(() => {
     axios.get("https://aircall-job.herokuapp.com/activities")
       .then(res => {
@@ -17,10 +27,21 @@ const App = () => {
       .catch(err => console.log(err));
   }, []);
 
+  // States to select current view
+  const [view, setView] = useState(0)
+
+  // onChange function to switch views
+  const handleChange = (event, newValue) => {
+    setView(newValue);
+  };
+
+
   return (
     <div className='container'>
-      <Header/>
+      <Header view={view} handleChange={handleChange}/>
       <div className="container-view">
+        
+        
         <CallList callLogs={callLogs} />
       </div>
     </div>
